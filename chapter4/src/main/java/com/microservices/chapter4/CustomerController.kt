ac694cefb63d1515beff7352516908225080a0da
seogi1004 <seogi777@gmail.com>
2019-05-01 15:11:41 +0900

    4장 p.213까지

--- chapter4/src/main/java/com/microservices/chapter4/CustomerController.kt
+++ chapter4/src/main/java/com/microservices/chapter4/CustomerController.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
 
 @RestController
 class CustomerController {
@@ -11,7 +12,7 @@ class CustomerController {
     private lateinit var customerService: CustomerService
 
     @GetMapping(value = [ "/customer/{id}" ])
-    fun getCustomer(@PathVariable id: Int): ResponseEntity<Customer?> {
+    fun getCustomer(@PathVariable id: Int): ResponseEntity<Mono<Customer>?> {
         val customer = customerService.getCustomer(id)
         return ResponseEntity(customer, HttpStatus.OK)
     }
@@ -19,4 +20,8 @@ class CustomerController {
     @GetMapping(value = [ "/customers" ])
     fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
             customerService.searchCustomers(nameFilter)
+
+    @PostMapping(value = [ "/customer/" ])
+    fun createCustomer(@RequestBody customerMono: Mono<Customer>) =
+            ResponseEntity(customerService.createCustomer(customerMono), HttpStatus.CREATED)
 }
--- chapter4/src/main/java/com/microservices/chapter4/CustomerServiceImpl.kt
+++ chapter4/src/main/java/com/microservices/chapter4/CustomerServiceImpl.kt
@@ -1,6 +1,10 @@
 package com.microservices.chapter4
 
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toFlux
+import reactor.core.publisher.toMono
 import java.util.concurrent.ConcurrentHashMap
 
 @Component
@@ -15,10 +19,16 @@ class CustomerServiceImpl : CustomerService {
 
     val customers = ConcurrentHashMap<Int, Customer>(initializerCustomers.associateBy(Customer::id))
 
-    override fun getCustomer(id: Int) = customers[id]
+    override fun getCustomer(id: Int) = customers[id]?.toMono() ?: Mono.empty()
 
-    override fun searchCustomers(nameFilter: String): List<Customer> =
+    override fun searchCustomers(nameFilter: String): Flux<Customer> =
             customers.filter {
                 it.value.name.contains(nameFilter, true)
-            }.map(Map.Entry<Int, Customer>::value).toList()
+            }.map(Map.Entry<Int, Customer>::value).toFlux()
+
+    override fun createCustomer(customerMono: Mono<Customer>): Mono<*> =
+        customerMono.map {
+            customers[it.id] = it
+            Mono.empty<Any>()
+        }
 }
