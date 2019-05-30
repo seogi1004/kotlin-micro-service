757b7d82345b4c17c3a171f70c09bed6f5e092e3
alvin <seogi777@gmail.com>
2019-05-30 17:12:55 +0900

    6장 코드 작성 중...

---
+++ chapter6-microservice/src/main/kotlin/com/microservices/chapter6microservice/Chapter6MicroServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.microservices.chapter6microservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Chapter6MicroServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<Chapter6MicroServiceApplication>(*args)
+}
---
+++ chapter6-microservice/src/test/kotlin/com/microservices/chapter6microservice/Chapter6MicroserviceApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.microservices.chapter6microservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Chapter6MicroserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
